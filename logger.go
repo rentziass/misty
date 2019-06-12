2b231eb75a59e4d9858a4df009624020690c5468
Francesco Renzi <rentziass@gmail.com>
2019-06-12 12:28:49 +0100

    Extract Logger

--- logger.go
+++ logger.go
@@ -1,1 +1,15 @@
 package misty
+
+type Logger interface {
+	Info(...interface{})
+	Debug(...interface{})
+	Warn(...interface{})
+	Error(...interface{})
+}
+
+type emptyLogger struct{}
+
+func (emptyLogger) Info(...interface{})  {}
+func (emptyLogger) Debug(...interface{}) {}
+func (emptyLogger) Warn(...interface{})  {}
+func (emptyLogger) Error(...interface{}) {}
